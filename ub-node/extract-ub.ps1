41456579c56a3fe7ad9dcf22863f7db1497f80b1
Stuart Shay <sshay@yahoo.com>
2016-07-17 16:34:24 -0400

    Update Script

--- ub-node/extract-ub.ps1
+++ ub-node/extract-ub.ps1
@@ -4,8 +4,8 @@ $ResourceGroupName = 'AzureRM'
 $StorageAccountName = "azurestoragez1"
 
 $i = 1
-$vmName = 'ub-client-$i'
-$nicName = = 'ub-client-nic-$i'
+$vmName = 'ub-client-'+ $i
+$nicName = 'ub-client-nic-' + $i
 $diskName = $vmName + '-os-disk.vhd';
 
 $vhdNamePrefix = 'ub-16.04.0-lts-16-07-13'
@@ -17,8 +17,9 @@ Set-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $vmName -Generalized
 Save-AzureRmVMImage -ResourceGroupName $ResourceGroupName -Name $vmName -DestinationContainerName "vm-images" -VHDNamePrefix $vhdNamePrefix -Path $path
 
 # Remove Resources
-Remove-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $ResourceGroupName -Force 
 Remove-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $vmName
+Remove-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $ResourceGroupName -Force 
+
 
 #Remove VHD Disk
 $storageContext = (Get-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -AccountName $StorageAccountName).Context
